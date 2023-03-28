f6bbbe5192474fc58f2aee37a66923f70abd65e4
fat-alpacas <78932452+fat-alpacas@users.noreply.github.com>
2023-03-28 14:43:46 -0700

    Add files via upload

---
+++ HerbAlpert.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class HerbAlpert {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    boolean quit = false;
+
+    while (!quit) {
+      System.out.println("1 Overview");
+      System.out.println("2 Early life and career");
+      System.out.println("3 The Tijuana Brass years");
+      System.out.println("4 Post-Brass musical career");
+      System.out.println("5 Philanthropy");
+      System.out.println("6 Personal life");
+      System.out.println("7 quit");
+      System.out.println("Enter choice");
+      String choice = scanner.nextLine() .toLowerCase();
+      
+      switch (choice) {
+        case "1":
+          System.out.println("Herb Alpert (born March 31, 1935) is an American trumpeter who led the band Herb Alpert & the Tijuana Brass in the 1960s. During the same decade, he co-founded A&M Records with Jerry Moss. Alpert has recorded 28 albums that have landed on the Billboard 200 chart, five of which became No. 1 albums; he has had 14 platinum albums and 15 gold albums. Alpert is the only musician to hit No. 1 on the U.S. Billboard Hot 100 as both a vocalist This Guy's in Love with You, 1968 and an instrumentalist , 1979). Alpert has reportedly sold 72 million records worldwide.[1] He has received many accolades, including a Tony Award and eight Grammy Awards,[2] as well as the Grammy Lifetime Achievement Award. In 2006, he was inducted into the Rock and Roll Hall of Fame. Alpert was awarded the National Medal of Arts by Barack Obama in 2013.");
+          break;
+
+        case "2":
+        System.out.println("Test"); 
+        System.out.println("Herb Alpert was born and raised in the Boyle Heights section of Eastside Los Angeles, California, the youngest of three children (a daughter and two sons) of Tillie (née Goldberg) and Louis Leib (or Louis Bentsion-Leib) Alpert. His parents were Jewish immigrants to the U.S. from Radomyshl (in present-day Ukraine) and Romania. Alpert was born into a family of musicians. His father, although a tailor by trade, was also a talented mandolin player. His mother taught violin at a young age, and his older brother, David, was a talented young drummer. His sister Mimi, who was the oldest, played the piano. Herb began to play trumpet at eight years old. Herb started attending Fairfax High School in Los Angeles beginning there in the 10th grade for the Class of 1951. In the 11th grade in 1952, he was a member of their Gym Team, where one of his specialties was performing on the Rings, but an appendectomy a week prior to a League Meet sidelined his path to continue there. It was in his Senior year (1953), he took to focusing on his trumpet. While attending the University of Southern California in the 1950s, he was a member of the USC Trojan Marching Band for two years. In 1956, he appeared in an uncredited role as Drummer on Mt. Sinai in The Ten Commandments. In 1957, Alpert teamed up with Rob Weerts, another burgeoning lyricist, as a songwriter for Keen Records. A number of songs written or co-written by Alpert during the following two years became Top 20 hits, including Baby Talk by Jan and Dean and Wonderful World by Sam Cooke. In 1960, he began his recording career as a vocalist at RCA Records under the name of Dore Alpert. In 1962, Alpert and his new business partner Jerry Moss formed Carnival Records with Tell It to the Birds as its first release, distribution outside of Los Angeles being done by Dot Records. After Carnival released its second single Love Is Back In Style by Charlie Robinson, Alpert and Moss found that there was prior usage of the Carnival name and renamed their label A&M Records.");
+          
+          break;
+
+        case "3":
+          System.out.println("The song that jump-started Alpert's performing career was originally titled Twinkle Star, written by Sol Lake (who would write many Tijuana Brass songs over the next decade). Alpert was dissatisfied with his first efforts to record the song, then took a break to visit a bullfight in Tijuana, Mexico. As Alpert later recounted, That's when it hit me! Something in the excitement of the crowd, the traditional mariachi music, the trumpet call heralding the start of the fight, the yelling, the snorting of the bulls, it all clicked. Alpert adapted the trumpet style to the tune, mixed in crowd cheers and other noises for ambience, and renamed the song The Lonely Bull. He personally funded the production of the record as a single, and it spread through radio DJs until it caught on and became a Top 10 hit in the Fall of 1962. He followed up quickly with his debut album, The Lonely Bull by Herb Alpert & the Tijuana Brass. Originally the Tijuana Brass was just Alpert overdubbing his own trumpet, slightly out of sync. It was A&M's first album (with the original release number being #101), although it was recorded for Conway Records. The title cut reached No. 6 on the Billboard pop chart. For this album and subsequent releases, Alpert recorded with the group of Los Angeles session musicians known as the Wrecking Crew, whom he holds in high regard. Alpert in 1966 Alpert’s 1965 album Whipped Cream & Other Delights proved so popular — it was the number one album of 1966, outselling The Beatles, Frank Sinatra, and The Rolling Stones — that Alpert had to turn the Tijuana Brass into an actual touring ensemble rather than a studio band. Some of that popularity might be attributable to the album’s notoriously racy cover, which featured model Dolores Erickson seemingly clothed only in whipped cream. However, as writer Bruce Handy pointed out in a Billboard article, two other Brass albums, Going Places (1965) and What Now My Love (1966), held the third and fifth spots on the 1966 year-end chart despite pleasant yet far more anodyne covers. Another measure of the band’s popularity is that a number of Tijuana Brass songs were used as theme music for years by the ABC TV game show, The Dating Game. In 1966, a short animated film by John and Faith Hubley called A Herb Alpert and the Tijuana Brass Double Feature was released; it won the Academy Award for Best Animated Short Film in 1967. The film featured two songs by the band, Tijuana Taxi and Spanish Flea. Also in 1967, the Tijuana Brass performed Burt Bacharach's title cut to the first movie version of Casino Royale. Alpert's only No. 1 single during this period, and the first No. 1 hit for his A&M label, was a solo effort: This Guy's in Love with You , written by Burt Bacharach and Hal David, featuring a rare vocal Alpert sang it to his first wife in a 1968 CBS Television special titled Beat of the Brass. The sequence was filmed on the beach in Malibu. The song was not intended to be released, but after it was used in the television special, allegedly thousands of telephone calls to CBS asking about it convinced Alpert to release it as a single, two days after the show aired.Although Alpert's vocal skills and range were limited, the song's technical demands suited him. After years of success, Alpert had a personal crisis in 1969, declaring the trumpet is my enemy. He disbanded the Tijuana Brass, and stopped performing in public. Eventually he sought out teacher Carmine Caruso, who never played trumpet a day in his life, (but) he was a great trumpet teacher. What I found, Alpert told The New York Times, is that the thing in my hands is just a piece of plumbing. The real instrument is me, the emotions, not my lip, not my technique, but feelings I learned to stuff away -- as a kid who came from a very unvocal household. Since then, I've been continually working it out, practicing religiously and now, playing better than ever. The results were noticeable; as Richard S. Ginell wrote in an AllMusic review of Alpert's comeback album, You Smile - The Song Begins, His four-year sabbatical over, Herb Alpert returned to the studio creatively refreshed, his trumpet sounding more soulful and thoughtful, his ears attuned more than ever to jazz.");
+          break;
+
+        case "4":
+          System.out.println("In 1979, five years after his last chart hit with the Tijuana Brass, Alpert tried to record a disco album of rearranged Brass hits. It just sounded awful to me, Alpert was quoted later. I didn’t want any part of it. But because the musicians were already booked, Alpert recorded other material, including the instrumental Rise, co-written by his nephew, Randy Badazz Alpert. The song hit number one on the Billboard Hot 100 after it was used repeatedly on the soap opera General Hospital. The song also became a hit in the UK, but in a speeded-up version, due to British DJs not realizing that the American 12 single was recorded at 33 rpm instead of 45 rpm. In 2013, Alpert released Steppin' Out, which won a Grammy for Best Pop Instrumental Album.");
+          break;
+        case "5":
+          System.out.println("In the 1980s Alpert created the Herb Alpert Foundation and the Alpert Awards in the Arts with the California Institute of the Arts (CalArts). The Foundation supports youth and arts education as well as environmental issues and helps fund the PBS series Bill Moyers on Faith and Reason and later Moyers & Company. Alpert and his wife donated $30 million to University of California, Los Angeles in 2007, to form and endow the UCLA Herb Alpert School of Music as part of the restructured UCLA School of the Arts and Architecture. He gave $24 million, which included $15 million from April 2008, to CalArts for its music curricula, and provided funding for the culture jamming activists The Yes Men. In 2012, the Foundation gave a grant of more than $5 million to the Harlem School of the Arts, which allowed the school to retire its debt, restore its endowment, and create a scholarship program for needy students; in 2013, the school's building was renamed the Herb Alpert Center. In 2016, his foundation also made a $10.1 million donation to Los Angeles City College that will provide all music majors at the school with a tuition-free education, beginning in fall of 2017. This was the largest gift to an individual community college in the history of Southern California, and the second-largest gift in the history of the state In 2020, Alpert bestowed an additional $9.7 million on the Harlem School of the Arts to upgrade its facility. He founded the Louis and Tillie Alpert Music Center in Jerusalem, which brings together both Arab and Jewish students.");
+          break;
+        case "6":
+          System.out.println("Alpert married Sharon Mae Lubin at Presidio of San Francisco in 1956. They had two children, Dore (born 1960) and Eden (born 1966). The couple divorced in 1971. Two years later, Alpert married Lani Hall, once the lead singer of A&M group Brasil ’66. Alpert and Hall have a daughter, Aria, born in 1976. Hall and Alpert recorded a live album, Anything Goes, in 2009; a studio album, I Feel You, in 2011; and another studio album, Steppin' Out, in 2013. As Matt Collar wrote in AllMusic, Ultimately, Steppin' Out represents not just the third album in a trilogy, but a loving creative partnership that, for Alpert and Hall, spans a lifetime.");
+          break;
+        case "7":
+          quit = true;
+          break;
+        case "a&m":
+        System.out.println("Herb Alpert is secretly rich becasuse he sold A&M records for 700 00 000 dollers");
+        default:
+          System.out.println("invalade choose");
+          break;
+      }
+
+      
+    }
+    scanner.close();
+  }
+}
