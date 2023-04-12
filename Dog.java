38c039c2228936397d023aa931d237aed23bbb79
fat-alpacas <78932452+fat-alpacas@users.noreply.github.com>
2023-04-12 08:42:18 -0700

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,49 @@
+
+    class Dog
+    {
+        // instance variables
+        private String breed;
+        private String name;
+        private int age;
+    
+        public Dog()
+        {
+            breed = "";
+            name = "";
+            age = 0;
+        }
+    
+        public Dog(String b)
+        {
+            breed = b;
+            name = "";
+            age = 0;
+        }
+    
+        public Dog(String b, String n)
+        {
+            breed = b;
+            name = n;
+            age = 0;
+        }
+    
+        public Dog(String b, String n, int a)
+        {
+            breed = b;
+            name = n;
+            age = a;
+        }
+    }
+    
+
+    public class Dogtype
+{
+    public static void main(String [] args)
+    {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog("Golden Retriever");
+        Dog dog3 = new Dog("Collie", "Lassie");
+        Dog dog4 = new Dog("German Shepherd", "Max", 3);
+    }
+}
+    
