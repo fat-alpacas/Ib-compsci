d1cb0d73fc0d67b20c01d82c0e90533963de7a3e
fat-alpacas <78932452+fat-alpacas@users.noreply.github.com>
2023-05-01 12:30:46 -0700

    Add files via upload

--- Dog.java
+++ Dog.java
@@ -1,51 +1,78 @@
-class Dog
-{
-    // instance variables
+import java.io.*;
+
+import javax.imageio.stream.ImageInputStream;
+
+public class Dog {
+
     private String breed;
     private String name;
     private int age;
+    private int numDogs;
 
-    public Dog()
-    {
+    public Dog() {
         breed = "";
         name = "";
         age = 0;
     }
 
-    public Dog(String b)
-    {
+    public Dog(String b) {
         breed = b;
         name = "";
         age = 0;
     }
 
-    public Dog(String b, String n)
-    {
+    public Dog(String b, String n) {
         breed = b;
         name = n;
         age = 0;
     }
 
-    public Dog(String b, String n, int a)
-    {
+    public Dog(String b, String n, int a) {
         breed = b;
         name = n;
         age = a;
     }
-System.out.println(dog1);
-System.out.println(dog2);
-System.out.println(dog3);
-System.out.println(dog4);
-}
 
+    public String toString() {
+        return "Breed: " + breed + ", Name: " + name + ", Age: " + age;
+    }
 
-public class Dogtype
-{
-public static void main(String [] args)
-{
-    Dog dog1 = new Dog();
-    Dog dog2 = new Dog("Golden Retriever");
-    Dog dog3 = new Dog("Collie", "Lassie");
-    Dog dog4 = new Dog("German Shepherd", "Max", 3);
+    public void Wright(String filename) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(filename));
+            writer.println(this.toString());
+            writer.close();
+            System.out.println("Data written to " + filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void maim(String[]args){
+        BufferedReader reader = new BufferedReader((Reader) new ImageInputStream(System.in)); {
+            int numDogs = 0;
+            System.out.println("How Many dogs?");
+            try{
+                numDogs = Integer.parseInt(reader.readLine());
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    
+    
+    }
+    Dog[] dogs = new Dog[numDogs];{
+    for (int i = 0; i < numDogs; i++) {
+        System.out.println("enter dog breed"+ (i+1) + ":");
+        String breed = "";
+        try{
+            breed = Reader.readLine();
+        }    catch (IOException e){
+                e.printStackTrace();
+            }
+        dogs[i] = new Dog(breed);
+        System.out.println("files printed");
+        }
+    
 }
 }
