971a8c62e84047a64c7bce623cdc4f65c34ac1fb
fat-alpacas <78932452+fat-alpacas@users.noreply.github.com>
2023-05-30 13:39:09 -0700

    Add files via upload

--- Rolldice.java
+++ Rolldice.java
@@ -4,29 +4,45 @@ import java.util.Random;
 public class Rolldice {
 
     public static void main (String[] args){
-        class die{
+       class die{
             int faceValue;
+            int faceValue2;
+            public void roll(){
 
-            public void roll()
-
-            {
+            
                 faceValue = (int)(Math.random()*6+1);
-
-                System.out.println("the value is" + faceValue);
+                faceValue2 = (int)(Math.random()*6+1);
+                System.out.println("the value is " + faceValue + " And " + faceValue2);
             }
+     
+     public int getfaceValue(){
+        return faceValue;
+     }    
         }
             die firstroll = new die ();
-
             die secoundroll = new die();
             die thirdroll = new die();
             die fourthroll = new die();
             die fithroll = new die();
+        int rollcount = 0;
+        while (true){
+            
+          int firstValue = faceValue.getfaceValue();
+          int secoundValue = faceValue2.getfaceValue();
+         
 
-            firstroll.roll();
-            secoundroll.roll();
-            thirdroll.roll();
-            fourthroll.roll();
-            fithroll.roll();
-
+        
+            
+            System.out.println("First roll was " + firstroll);
+            System.out.println("Secound roll was " + secoundValue);
+            System.out.println("So far you have rolled " + rollcount + "times");
+        
+            rollcount++;
+        }
+    if (firstValue == secoundValue) {
+        System.out.println("The rolls are the same and it only took" + rollcount + "times" );
     }
+    
+
+}
 }
